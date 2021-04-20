388cefa213221149c48f20fa795940eec426c2f9
msinkec <mihael@sinkec.xyz>
2021-04-20 10:08:44 +0200

    Implemented compiler error handling.

--- scryptlib/compiler_wrapper.py
+++ scryptlib/compiler_wrapper.py
@@ -1,6 +1,56 @@
+import re
 import subprocess
 from datetime import datetime
 from pathlib import Path
+
+
+SYNTAX_ERR_REG = '(?P<filePath>[^\s]+):(?P<line>\d+):(?P<column>\d+):\n([^\n]+\n){3}' \
+        '(unexpected (?P<unexpected>[^\n]+)\nexpecting (?P<expecting>[^\n]+)|(?P<message>[^\n]+))'
+SEMANTIC_ERR_REG = 'Error:(\s|\n)*(?P<filePath>[^\s]+):(?P<line>\d+):(?P<column>\d+):' \
+        '(?P<line1>\d+):(?P<column1>\d+):*\n(?P<message>[^\n]+)\n'
+INTERNAL_ERR_REG = 'Internal error:(?P<message>.+)'
+WARNING_REG = 'Warning:(\s|\n)*(?P<filePath>[^\s]+):(?P<line>\d+):(?P<column>\d+):' \
+        '(?P<line1>\d+):(?P<column1>\d+):*\n(?P<message>[^\n]+)\n'
+
+
+class SyntaxErrorEntry:
+    def __init__(self, message, message_full, got, expected, position, file_path):
+        self.message = message
+        self.message_full = message_full
+        self.got = got
+        self.expected = expected
+        self.file_path = file_path
+        self.position = position # (line, col)
+
+
+class SemanticErrorEntry:
+    def __init__(self, message, message_full, position_range, file_path):
+        self.message = message
+        self.message_full = message_full
+        self.file_path = file_path
+        self.position_range = position_range # [(line, col), (line1, col1)]
+
+
+class EntryErrorBase(Exception):
+    def __init__(self, error_entries):
+        self.error_entries = error_entries
+        message = []
+        for error_entry in self.error_entries:
+            message.append(error_entry.message_full)
+        message = '\n'.join(message)
+        super().__init__(message) 
+
+
+class SyntaxError(EntryErrorBase):
+    pass
+
+
+class SemanticError(EntryErrorBase):
+    pass
+
+
+class InternalError(Exception):
+    pass
 
 
 def compile(source, **kwargs):
@@ -13,6 +63,8 @@ def compile(source, **kwargs):
     timeout = 1200
     out_files = dict()
     cmd_args = None
+    from_file = isinstance(source, Path)
+    cwd = Path('.')
 
     if 'asm' in kwargs:
         asm = kwargs['asm']
@@ -28,10 +80,8 @@ def compile(source, **kwargs):
         timeout = kwargs['timeout']
     if 'cmd_args' in kwargs:
         cmd_args = kwargs['cmd_args']
-
-    if not 'cwd' in kwargs:
-        raise Exception('Missing argument "cwd". No working directory specified for compiler command.')
-    cwd = kwargs['cwd']
+    if 'cwd' in kwargs:
+        cwd = Path(kwargs['cwd'])
 
     if not 'out_dir' in kwargs:
         raise Exception('Missing argument "out_dir". No output directory specified.')
@@ -56,34 +106,100 @@ def compile(source, **kwargs):
     cmd_buff.append('{}'.format(str(out_dir.absolute())))
     if cmd_args:
         cmd_buff.append(cmd_args)
-
-    print(cmd_buff)
-    print(cwd)
+    if from_file:
+        cmd_buff.append(str(source))
 
     # Execute compiler
     res = subprocess.run(cmd_buff, 
             stdout=subprocess.PIPE, 
-            input=source.encode('utf-8'),
+            input=None if from_file else source.encode('utf-8'),
             timeout=timeout,
             cwd=cwd
-            ).stdout
+        ).stdout
 
     # If compiling on win32, the outputs will be CRLF seperated.
     # We replace CRLF with LF, to make SYNTAX_ERR_REG、SEMANTIC_ERR_REG、IMPORT_ERR_REG work.
-    res = res.replace(b'\r\n', b'\n')
+    res = res.replace(b'\r\n', b'\n').decode('utf-8')
+
+    # Check compiler output for errors and raise exception if needed.
+    __check_for_errors(res)
+
+    # Collect warnings from compiler output.
+    warnings = __get_warnings(res)
+
+    # TODO
+
+def __check_for_errors(compiler_output):
+    if compiler_output.startswith('Error:'):
+        match = re.search(INTERNAL_ERR_REG, compiler_output)
+        if match:
+            raise InternalError('Compiler internal error: {}'.format(match.group('message')))
+
+        syntax_err_entries = []
+        for match in re.finditer(SYNTAX_ERR_REG, compiler_output):
+            file_path = match.group('filePath')
+            got = match.group('unexpected')
+            expected = match.group('expecting')
+            line = int(match.group('line'))
+            col = int(match.group('column'))
+            message = match.group('message')
+            message_full = match.string
+
+            error_entry = SyntaxErrorEntry(message, message_full, got, expected, (line, col), file_path)
+            syntax_err_entries.append(error_entry)
+
+        if len(syntax_err_entries) > 0:
+            raise SyntaxError(syntax_err_entries)
+
+        semantic_err_entries = []
+        for match in re.finditer(SEMANTIC_ERR_REG, compiler_output):
+            file_path = match.group('filePath')
+            line = int(match.group('line'))
+            col = int(match.group('column'))
+            line1 = int(match.group('line1'))
+            col1 = int(match.group('column1'))
+            position_range = [(line, col), (line1, col1)]
+            message = match.group('message')
+            message_sub_reg = 'Symbol `(?P<varName>\w+)` already defined at (?P<fileIndex>[^\s]+)' \
+                    ':(?P<line>\d+):(?P<column>\d+):(?P<line1>\d+):(?P<column1>\d+)'
+            message = re.sub(message_sub_reg, 'Symbol `$1` already defined at $3:$4:$5:$6', message)
+            message_full = match.string
+
+            error_entry = SemanticErrorEntry(message, message_full, position_range, file_path)
+            semantic_err_entries.append(error_entry)
+
+        if len(semantic_err_entries) > 0:
+            raise SemanticError(semantic_err_entries)
+
+        raise Exception(compiler_output)
+
+
+def __get_warnings(compiler_output):
+    warnings = []
+    for match in  re.finditer(WARNING_REG, compiler_output):
+        file_path = match.group('filePath')
+
+        line = int(match.group('line'))
+        col = int(match.group('column'))
+        line1 = int(match.group('line1'))
+        col1 = int(match.group('column1'))
+
+        message = match.group('message')
+        message_sub_reg = 'Variable `(?<varName>\w+)` shadows existing binding at ' \
+                '(?<fileIndex>[^\s]+):(?<line>\d+):(?<column>\d+):(?<line1>\d+):(?<column1>\d+)'
+        message = re.sub(message_sub_reg, 'Variable `$1` shadows existing binding at $3:$4:$5:$6', message)
+
+        warnings.append((file_path, [(line, col), (line1,col1)], message))
+    return warnings
 
 
 def compile_f(source_path, **kwargs):
-    with open(source_path, 'r', encoding='utf-8') as f:
-        source = f.read()
+    source_path = Path(source_path)
 
     if not 'out_dir' in kwargs:
-        kwargs['out_dir'] = Path(source_path).parent
+        kwargs['out_dir'] = source_path.parent
 
-    if not 'cwd' in kwargs:
-        kwargs['cwd'] = Path(source_path).parent
-
-    compile(source, **kwargs)
+    compile(source_path, **kwargs)
 
 
 
